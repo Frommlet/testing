73bc5e00293afe830baf2bef1302c713ec2f6418
Aaron Schuman <aaron@aschuman.net>
2014-04-11 15:29:59 -0700

    Create aa_interactive.sh
    Spruced up the things.

---
+++ aa_interactive.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Script with interaction, definitely.
+
+# Set sane default value
+var1 = "n"
+
+# Prompt user and read in their input
+echo "Hello $USER, would you like to know your system information? (y/n)"
+read var1
+
+# If user wants info call system_profiler, otherwise print passive-aggressive message
+if [ $var1 == "y" ]
+	then
+		system_profiler
+else
+	echo "Fine, see if I care."
+fi
